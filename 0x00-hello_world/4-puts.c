641bb44e975bcb8f19c570b3593b452626db76e5
ELMEHDIBOUDIAB <elmehdiboudiab@gmail.com>
2023-08-10 06:54:37 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
- *
- * Return: Always 0 (Success)
+ * Hey C
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
