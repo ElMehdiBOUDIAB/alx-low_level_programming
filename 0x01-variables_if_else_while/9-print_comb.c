475eb1af347e5fb8d0c3498b2a7679592d6e6329
ELMEHDIBOUDIAB <elmehdiboudiab@gmail.com>
2023-08-14 09:00:18 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,12 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
+		if (i != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-	return (0);
+		putchar('\n');
+		return (0);
 }
