33ef3ed5f6ece5f3bbfac92be83df0fa2359a855
ELMEHDIBOUDIAB <elmehdiboudiab@gmail.com>
2023-08-15 14:53:29 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,47 @@
 #include "main.h"
-
 /**
- * print_to_98 - print n to 98 counts
- *             seperated by comma, followed
- *             by space and number should be
- *             printed in order
- *
- * @n: input
-*/
-
-void print_to_98(int n)
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
 {
-	if (n <= 98)
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (n = n; n <= 97; n++)
-			printf("%d. ", n);
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
 	}
-	else
-		for (n = n; n > 98; n--)
-			printf("%d. ", n);
-	printf("98\n");
 }
+
